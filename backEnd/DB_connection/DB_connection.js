8fbab29631cc03127f2cb473333875965a63192d
Nitin-Lohumi <lohuminitin@gmail.com>
2024-12-24 17:20:19 +0530

    add

--- backEnd/DB_connection/DB_connection.js
+++ backEnd/DB_connection/DB_connection.js
@@ -1,8 +1,8 @@
-const mongoose = require("mongoose");
-try {
-  mongoose.connect(process.env.DB_URL);
-  console.log("connected sucessfull");
-} catch (error) {
-  console.log(error);
-  throw error;
-}
+// const mongoose = require("mongoose");
+// try {
+//   mongoose.connect(process.env.DB_URL);
+//   console.log("connected sucessfull");
+// } catch (error) {
+//   console.log(error);
+//   throw error;
+// }
--- backEnd/config/CloudinaryConfig.js
+++ backEnd/config/CloudinaryConfig.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
-const { v2: cloudinary } = require('cloudinary');
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME, // Your Cloudinary cloud name
-    api_key: process.env.CLOUDINARY_API_KEY, // Your Cloudinary API key
-    api_secret: process.env.CLOUDINARY_API_SECRET, // Your Cloudinary API secret
-});
-module.exports = cloudinary;
+// require('dotenv').config();
+// const { v2: cloudinary } = require('cloudinary');
+// cloudinary.config({
+//     cloud_name: process.env.CLOUDINARY_NAME, // Your Cloudinary cloud name
+//     api_key: process.env.CLOUDINARY_API_KEY, // Your Cloudinary API key
+//     api_secret: process.env.CLOUDINARY_API_SECRET, // Your Cloudinary API secret
+// });
+// module.exports = cloudinary;
