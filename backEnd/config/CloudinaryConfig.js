c1ee228ccf0d370f96fd41b02aef8120ce6ad512
Nitin-Lohumi <lohuminitin@gmail.com>
2024-12-24 17:30:42 +0530

    add

--- backEnd/config/CloudinaryConfig.js
+++ backEnd/config/CloudinaryConfig.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const { v2: cloudinary } = require('cloudinary');
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME, // Your Cloudinary cloud name
-    api_key: process.env.CLOUDINARY_API_KEY, // Your Cloudinary API key
-    api_secret: process.env.CLOUDINARY_API_SECRET, // Your Cloudinary API secret
+    cloud_name: process.env.CLOUDINARY_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET, 
 });
 module.exports = cloudinary;
